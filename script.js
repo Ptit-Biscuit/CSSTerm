18dd86b383ea26a873bae5440e18568c7c3df297
Ptit-Biscuit <vincent.brebion@epsi.fr>
2019-05-08 18:15:05 +0200

    Removed unnecessary things

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 function read(e) {
   if (e.keyCode === 13) {
-    var command = $("#command").val();
-    if (command != "") {
+    var command = $('#command').val();
+    if (command != '') {
       readConsole(command);
-      $("#command").val("");
+      $('#command').val('');
     }
   }
 }
 
 function readConsole(command) {
-  switch (command) {
-    case "help":
-      writeConsole(
-        '<span style="color: red;">Command: "' + command + '" not found</span>'
-      );
-      break;
-
-    default:
-      writeConsole(
-        '<span style="color: red;">Command: "' + command + '" not found</span>'
-      );
-  }
+  writeConsole(command);
 }
 
 function writeConsole(command) {
-  $("input").html(command);
+  document.querySelector('.term-output').innerHTML = '<span class="output">' + command + '</span>';
 }
