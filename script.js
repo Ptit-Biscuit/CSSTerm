9125d20a6e0e9f507126c22e02f9deacf9502d34
Ptit-Biscuit <vincent.brebion@epsi.fr>
2019-05-09 23:14:14 +0200

    Greatly improved thanks to EnzDev

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function read(e) {
-  if (e.keyCode === 13) {
-    var command = $('#command').val();
-    if (command != '') {
-      readConsole(command);
-      $('#command').val('');
-    }
-  }
-}
-
-function readConsole(command) {
-  writeConsole(command);
-}
-
-function writeConsole(command) {
-  document.querySelector('.term-output').innerHTML = '<span class="output">' + command + '</span>';
-}
+/**
+ * HERE GOES COMMANDS
+ */
