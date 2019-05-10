f80a969392b1786578d60e1a4b2500448b861dbb
Vincent Brebion <vbrebion@sodius.com>
2019-05-10 15:09:57 +0200

    Starting to implement commands

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 /**
  * HERE GOES COMMANDS
  */
+var commands = {};
+var cmdHist = [];
+
+commands.clear = () => {
+  if ($(".term-history")) {
+    $(".term-history").remove();
+  }
+};
+
+commands.help = () => {
+  console.log("help");
+};
+
+commands.use = (fun, args = "") => {
+  if (fun) {
+    cmdHist.push(fun);
+    if (fun in commands) {
+      return commands[fun](args);
+    } else {
+      return fun + " not found in commands";
+    }
+  }
+};
