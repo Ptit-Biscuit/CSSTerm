c0add10c8ddfb7b8f6c60037b92d1a5e87060e67
Ptit-Biscuit <vincent.brebion@epsi.fr>
2019-05-14 21:21:18 +0200

    Prettify + cmd history

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
  * HERE GOES COMMANDS
  */
 var commands = {};
+var cmdHist = ['a', 'b', 'c'];
 
 commands.clear = () => {
-  if ($(".term-history")) {
-    $(".term-history").remove();
+  if ($('.term-history')) {
+    $('.term-history').remove();
   }
 };
 
 commands.help = () => {
-  console.log("help");
+  console.log('help');
 };
 
-commands.use = (fun, args = "") => {
-  if (fun) {
-    cmdHist.push(fun);
-    if (fun in commands) {
-      return commands[fun](args);
-    } else {
-      return fun + " not found in commands";
-    }
+commands.use = (cmd, args = '') => {
+  if (cmd) {
+    cmdHist.push(cmd);
+
+    return cmd in commands
+      ? commands[cmd](args)
+      : cmd + ' not found in commands';
   }
 };
